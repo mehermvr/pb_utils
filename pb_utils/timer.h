9201c0378b5fecc73fb94851143713aa65d1f6b9
Meher Malladi <meher.malladi@uni-bonn.de>
2024-02-22 16:48:32 +0100

    add tick tock to timer

--- pb_utils/timer.h
+++ pb_utils/timer.h
@@ -12,17 +12,22 @@ private:
 
   std::string _label;
   TimePoint _start_time;
+  TimePoint _tick;
 
 public:
   Timer() : _label("Execution"), _start_time(Clock::now()) {}
   explicit Timer(const std::string &label)
-      : _label(label), _start_time(Clock::now()) {}
+      : _label(label), _start_time(Clock::now()), _tick(Clock::now()) {}
 
   /* default other stuff */
   Timer(const Timer &) = default;
   Timer(Timer &&) = default;
   Timer &operator=(const Timer &) = default;
   Timer &operator=(Timer &&) = default;
+
+  void tick() { _tick = Clock::now(); }
+  // returns the seconds since the tick(), otherwise since initialization
+  const double tock() const { return (Clock::now() - _tick).count(); }
 
   ~Timer() {
     auto end_time = Clock::now();
